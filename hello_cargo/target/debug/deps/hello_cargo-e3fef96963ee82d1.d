9f5af5e96ce3c8272a6dac1dfcdb79ea8ad02f8f
fancylettuce <eh.career29@gmail.com>
2023-01-23 19:54:28 -0600

    Add files via upload
    chapter 2

---
+++ hello_cargo/target/debug/deps/hello_cargo-e3fef96963ee82d1.d
@@ -0,0 +1,5 @@
+/home/fancylettuce/projects/hello_cargo/target/debug/deps/hello_cargo-e3fef96963ee82d1.rmeta: src/main.rs
+
+/home/fancylettuce/projects/hello_cargo/target/debug/deps/hello_cargo-e3fef96963ee82d1.d: src/main.rs
+
+src/main.rs:
