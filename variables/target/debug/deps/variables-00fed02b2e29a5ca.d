b8584d0d5b3a3a4975fa03265bdc6d341e0843c6
fancylettuce <eh.career29@gmail.com>
2023-01-23 20:02:28 -0600

    Add files via upload

---
+++ variables/target/debug/deps/variables-00fed02b2e29a5ca.d
@@ -0,0 +1,5 @@
+/home/fancylettuce/projects/variables/target/debug/deps/variables-00fed02b2e29a5ca: src/main.rs
+
+/home/fancylettuce/projects/variables/target/debug/deps/variables-00fed02b2e29a5ca.d: src/main.rs
+
+src/main.rs:
