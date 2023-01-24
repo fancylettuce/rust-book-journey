f546cf628b4499de1d119464d7ac3713f5b6993f
fancylettuce <eh.career29@gmail.com>
2023-01-23 19:55:47 -0600

    Add files via upload
    Chapter 3

---
+++ functions/target/debug/deps/functions-4d991407820ebeb8.d
@@ -0,0 +1,5 @@
+/home/fancylettuce/projects/functions/target/debug/deps/functions-4d991407820ebeb8: src/main.rs
+
+/home/fancylettuce/projects/functions/target/debug/deps/functions-4d991407820ebeb8.d: src/main.rs
+
+src/main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/fancylettuce/projects/functions/target/debug/functions: /home/fancylettuce/projects/functions/src/main.rs
